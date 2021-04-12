45e117dfb8635eaa6cab2958517bc91d2af2d29e
Arik Alexander <arik@cc.com>
2021-04-12 07:13:59 +0000

    fxes

--- survey.py
+++ survey.py
@@ -16,19 +16,26 @@ def excel_to_csv(excel_file):
                     header=True)
     return tmp_csv_file
 
-def handle_res_file(res_file, question_start, num_of_questions):
+def handle_res_file(res_file, question_start, num_of_questions, is_part1):
     with open(excel_to_csv(res_file)) as fin:
-        first = False
+        jump = True
         for line in fin:
-            if first:
-                first= False
-                continue
             line = line[:-1]
             tokens = line.split(',')
             email = tokens[3]
+            if '@' in email:
+                jump = False
+            if jump:
+                continue
             if email not in results:
                 answers = [-1 for i in range(num_of_questions)]
                 results[email] = {'answers': answers}
+            answers = results[email]['answers']
+            q_pos = 0 if is_part1 else len(answers)
+            for i in range(question_start, question_start + num_of_questions):
+                answers[q_pos] = tokens[i]
+                q_pos += 1
+
 
 def load_db():
     with open(excel_to_csv(survey_file)) as fin:
@@ -50,7 +57,7 @@ def load_db():
             answer = tokens[-1]
             questions.append({'category':category, 'sub_category':sub_category, 'question':question, 'weight':weight, 'answer':answer})
 
-    handle_res_file(res_part1_file, 9, 25)
+    handle_res_file(res_part1_file, 9, 25, True)
     
 load_db()
 
